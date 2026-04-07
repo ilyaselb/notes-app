8591acbab74a26b6cc5a06c25767cfac7cbe0127
ilyaselb <il.elboutaybi@com>
2026-04-07 06:55:40 +0100

    Initial project setup: React frontend & Axum backend

---
+++ backend/src/main.rs
@@ -0,0 +1,13 @@
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Backend running..." }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
